4fc45611ba8b32dd567dcd34ece43cd47c5007d0
Ayushi Shah <ayushi.shah@cygnetinfotech.com>
2023-02-08 13:04:13 +0530

    Check NGROK working

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Test using ngrok webhook
+         NGROK webhook worked
         </a>
       </header>
     </div>
